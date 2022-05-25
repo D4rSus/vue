1fddd914bca23e63e419414c6b2dc99b38e0ff15
D4rSus <101612393+D4rSus@users.noreply.github.com>
2022-05-25 10:34:36 +0900

    Add files via upload

---
+++ src/pages/MyHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src="https://pix-feed.com/wp-content/uploads/2019/08/2-11.jpg" class="img-fluid" alt="Responsive image">
+  </div>
+  <div class="position-absolute top-50 start-50 translate-middle" style="font-size: 60px; color: white">
+    <h1>Стация технического обслуживания</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyHome"
+}
+</script>
+<style>
+img {
+  position: absolute;
+  filter: brightness(50%);
+}
+</style>
